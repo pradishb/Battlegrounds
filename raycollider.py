d8780e493e0a752c1ce90f7d0ee56250ad2e9ed1
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2018-08-11 10:27:40 +0545

    Adds eyes position in ray collider

--- raycollider.py
+++ raycollider.py
@@ -1,10 +1,18 @@
 import direct.directbase.DirectStart
+from panda3d.bullet import BulletBoxShape, BulletGhostNode
 from pandac.PandaModules import *
 
 
 class RayCollider():
-    # setup collision stuff
+    # setup eyepos
+    eyepos = LineSegs()
+    eyepos.setColor(0, 1, 0, 1)
+    eyepos.drawTo(0, 0, 0)
+    eyepos.setThickness(30)
+    node = eyepos.create()
+    np = base.render.attachNewNode(node)
 
+    # setup collision stuff
     picker = CollisionTraverser()
     queue = CollisionHandlerQueue()
 
@@ -20,23 +28,19 @@ class RayCollider():
     picker.addCollider(pickerNP, queue)
 
     @staticmethod
-    def getObjectHit():  # mpos is the position of the mouse on the screen
+    def getObjectHit():
         RayCollider.pickerRay.setFromLens(base.camNode, 0, 0)
         RayCollider.picker.traverse(render)
         if RayCollider.queue.getNumEntries() > 0:
             RayCollider.queue.sortEntries()
-            RayCollider.pickedObj = RayCollider.queue.getEntry(0).getIntoNodePath()
-            # axis = loader.loadModel('zup-axis.egg')
-            # axis.reparentTo(render)
-            # point = self.queue.getEntry(0).getSurfacePoint(render)
-            # normal = self.queue.getEntry(0).getSurfaceNormal(render)
-            # axis.setPos(point)
-            # axis.lookAt(point + normal)
-            # axis.setScale(0.05)
-            return RayCollider.queue.getEntry(0).getSurfacePoint(render)
+
+            point = RayCollider.queue.getEntry(0).getSurfacePoint(render)
+            RayCollider.np.setPos(point)
+
+            return point
         pFrom = Point3()
         pTo = Point3()
         base.camLens.extrude((0, 0), pFrom, pTo)
-
         pTo = base.render.getRelativePoint(base.cam, pTo)
+
         return pTo
