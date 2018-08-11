05df2d9b78c29df5a012580c7296d674fdb87bae
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2018-08-11 10:28:09 +0545

    Uses terrain 2

--- game.py
+++ game.py
@@ -32,7 +32,7 @@ class GameEngine():
         self.world.setDebugNode(self.debugNP.node())
 
         # Terrain
-        visNP = base.loader.loadModel('models/terrain.egg')
+        visNP = base.loader.loadModel('models/terrain2.egg')
 
         geom = visNP.findAllMatches('**/+GeomNode').getPath(0).node().getGeom(0)
         mesh = BulletConvexHullShape()
