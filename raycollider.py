f109d82490154a36d63ef02ae1aedfbc2e96ffcd
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2018-08-11 09:28:19 +0545

    Improve raycollider object

--- raycollider.py
+++ raycollider.py
@@ -3,63 +3,40 @@ from pandac.PandaModules import *
 
 
 class RayCollider():
-    def __init__(self):
-        # setup collision stuff
+    # setup collision stuff
 
-        self.picker = CollisionTraverser()
-        self.queue = CollisionHandlerQueue()
+    picker = CollisionTraverser()
+    queue = CollisionHandlerQueue()
 
-        self.pickerNode = CollisionNode('mouseRay')
-        self.pickerNP = base.cam.attachNewNode(self.pickerNode)
+    pickerNode = CollisionNode('mouseRay')
+    pickerNP = base.cam.attachNewNode(pickerNode)
 
-        self.pickerNode.setFromCollideMask(GeomNode.getDefaultCollideMask())
+    pickerNode.setFromCollideMask(GeomNode.getDefaultCollideMask())
 
-        self.pickerRay = CollisionRay()
+    pickerRay = CollisionRay()
 
-        self.pickerNode.addSolid(self.pickerRay)
+    pickerNode.addSolid(pickerRay)
 
-        self.picker.addCollider(self.pickerNP, self.queue)
+    picker.addCollider(pickerNP, queue)
 
-        # this holds the object that has been picked
-        self.pickedObj = None
+    @staticmethod
+    def getObjectHit():  # mpos is the position of the mouse on the screen
+        RayCollider.pickerRay.setFromLens(base.camNode, 0, 0)
+        RayCollider.picker.traverse(render)
+        if RayCollider.queue.getNumEntries() > 0:
+            RayCollider.queue.sortEntries()
+            RayCollider.pickedObj = RayCollider.queue.getEntry(0).getIntoNodePath()
+            # axis = loader.loadModel('zup-axis.egg')
+            # axis.reparentTo(render)
+            # point = self.queue.getEntry(0).getSurfacePoint(render)
+            # normal = self.queue.getEntry(0).getSurfaceNormal(render)
+            # axis.setPos(point)
+            # axis.lookAt(point + normal)
+            # axis.setScale(0.05)
+            return RayCollider.queue.getEntry(0).getSurfacePoint(render)
+        pFrom = Point3()
+        pTo = Point3()
+        base.camLens.extrude((0, 0), pFrom, pTo)
 
-        base.accept('mouse1', self.printMe)
-
-    # this function is meant to flag an object as being somthing we can pick
-    def makePickable(self, newObj):
-        newObj.setTag('pickable', 'true')
-
-    # this function finds the closest object to the camera that has been hit by our ray
-    def getObjectHit(self, mpos):  # mpos is the position of the mouse on the screen
-        self.pickedObj = None  # be sure to reset this
-        self.pickerRay.setFromLens(base.camNode, 0, 0)
-        self.picker.traverse(render)
-        if self.queue.getNumEntries() > 0:
-            self.queue.sortEntries()
-            self.pickedObj = self.queue.getEntry(0).getIntoNodePath()
-
-            axis = loader.loadModel('zup-axis.egg')
-            axis.reparentTo(render)
-            point = self.queue.getEntry(0).getSurfacePoint(render)
-            normal = self.queue.getEntry(0).getSurfaceNormal(render)
-            axis.setPos(point)
-            axis.lookAt(point + normal)
-            axis.setScale(0.05)
-
-            parent = self.pickedObj.getParent()
-            self.pickedObj = None
-
-            while parent != render:
-                if parent.getTag('pickable') == 'true':
-                    self.pickedObj = parent
-                    return self.queue.getEntry(0).getSurfacePoint(render)
-                else:
-                    parent = parent.getParent()
-        return None
-
-    def getPickedObj(self):
-        return self.pickedObj
-
-    def printMe(self):
-        print(self.getObjectHit(base.mouseWatcherNode.getMouse()))
-        print(self.pickedObj)
+        pTo = base.render.getRelativePoint(base.cam, pTo)
+        return pTo
