cce7b07cf619a95bf119d4e331c05f1c06ce42f9
Srijan Tamrakar <tamrakarsrijan98@gmail.com>
2018-08-12 19:55:51 +0545

    changes to color of no connection as well as position set for health bar

--- gameui.py
+++ gameui.py
@@ -5,15 +5,13 @@ from direct.directbase.DirectStart import base
 class GameUI:
     font = base.loader.loadFont('./fonts/neuropol.ttf')
     font.setPixelsPerUnit(60)
-    health = OnscreenText(text="Health = 100", pos=(-.5, .5), font=font, scale=0.1, fg=(0, 0, 0, 255))
-    health.setColor(255, 255, 255, 255)
-    display = OnscreenText(text="No Connection", pos=(0, 0), font=font, scale=0.25, fg=(0, 0, 0, 255))
+    health = OnscreenText(text="Health = 100", pos=(-.85,0.9), font=font, scale=0.1, fg=(0, 0, 0, 1), bg=(1, 1, 1, 1))
+    display = OnscreenText(text="No Connection", pos=(0, 0), font=font, scale=0.25, fg=(1, 1, 1, 1))
     display.setColor(255, 255, 255, 255)
 
     @staticmethod
     def createDisplayUI(text):
-        display = OnscreenText(text=text, pos=(0, 0), font=GameUI.font, scale=0.25, fg=(0, 0, 0, 255))
-        display.setColor(255, 255, 255, 255)
+        display = OnscreenText(text=text, pos=(0, 0), font=GameUI.font, scale=0.25, fg=(1, 1, 1, 1))
         return display
 
     @staticmethod
