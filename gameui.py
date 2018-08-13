4bd63dd06dd564ca07e64fc679e988c1924f26fd
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2018-08-13 13:18:29 +0545

    Fixes white bg text position

--- gameui.py
+++ gameui.py
@@ -1,5 +1,6 @@
 from direct.gui.OnscreenText import OnscreenText
 from direct.directbase.DirectStart import base
+from panda3d.core import TextNode
 
 
 class GameUI:
@@ -13,6 +14,6 @@ class GameUI:
 
     @staticmethod
     def createWhiteBgUI(text):
-        display = OnscreenText(text=text, pos=(-.85, 0.9), font=GameUI.font, scale=0.1, fg=(0, 0, 0, 1),
-                                     bg=(1, 1, 1, 1))
+        display = OnscreenText(text=text, pos=(-1.32, 0.9), font=GameUI.font, scale=0.1, fg=(0, 0, 0, 1),
+                                     bg=(1, 1, 1, 1), align=TextNode.ALeft)
         return display
