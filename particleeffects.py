7b1ac1d43780c15c27091825aa1e3b96457faaeb
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2018-08-14 00:10:05 +0545

    Adds particle effect class

---
+++ particleeffects.py
@@ -0,0 +1,15 @@
+from panda3d.core import Filename
+from direct.particles.ParticleEffect import ParticleEffect
+
+
+class ParticleEffects:
+    base.enableParticles()
+    p = ParticleEffect()
+    p.cleanup()
+    p = ParticleEffect()
+    p.loadConfig(Filename('yellow.ptf'))
+    p.setH(-90)
+    p.setY(20)
+
+    def __init__(self, t):
+        ParticleEffects.p.start(t)
