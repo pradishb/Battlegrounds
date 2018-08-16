32d247423d4f121b1afc60f120823410a2d4d070
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2018-08-16 09:47:00 +0545

    Adds exit function in server and client

--- new_client.py
+++ new_client.py
@@ -1,3 +1,5 @@
+import sys
+
 from direct.showbase.DirectObject import DirectObject
 from panda3d.core import ClockObject
 from direct_gui import ClientGui
@@ -7,6 +9,7 @@ from client_network import ClientNetwork
 class Client(DirectObject):
     def __init__(self):
         DirectObject.__init__(self)
+        base.accept('escape', sys.exit, [0])
         base.setFrameRateMeter(True)
         globalClock.setFrameRate(60)
         globalClock.setMode(ClockObject.MLimited)
--- new_server.py
+++ new_server.py
@@ -1,3 +1,5 @@
+import sys
+
 from direct.showbase.DirectObject import DirectObject
 from panda3d.core import ClockObject
 from direct_gui import ServerGui
@@ -7,6 +9,7 @@ from server_network import ServerNetwork
 class Server(DirectObject):
     def __init__(self):
         DirectObject.__init__(self)
+        base.accept('escape', sys.exit, [0])
         base.setFrameRateMeter(True)
         globalClock.setFrameRate(60)
         globalClock.setMode(ClockObject.MLimited)
